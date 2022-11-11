678bb125715761789e3fbf202eb5f8835a5cfd16
hai <nguyenvuongngochai@gmail.com>
2022-11-11 15:43:47 +0700

    finish cau 1

--- 19429601_NguyenVuongNgocHai/ShippingService/src/main/java/com/example/ShippingService/rest/ShipperRestController.java
+++ 19429601_NguyenVuongNgocHai/ShippingService/src/main/java/com/example/ShippingService/rest/ShipperRestController.java
@@ -1,5 +1,34 @@
 package com.example.ShippingService.rest;
 
-public class ShipperRestController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ShippingService.entity.Shiper;
+import com.example.ShippingService.model.ShipperOfCustomer;
+import com.example.ShippingService.service.ShipperService;
+
+@RestController
+public class ShipperRestController {
+	@Autowired
+	private ShipperService shipperService;
+	@GetMapping("/shiper")
+    public List<Shiper> getShipers() {
+        return shipperService.findAll();
+    }
+
+
+    @PostMapping("/shiper")
+    public Shiper save(@RequestBody Shiper shiper) {
+        return shipperService.themshiper(shiper);
+    }
+    @GetMapping("/shiper/{id}")
+	public ShipperOfCustomer getshiOfCustomer(@PathVariable int id) {
+		return shipperService.getShipperOfCustomer(id);
+	}
 }
